0a3be28407f5cfc7b2c581363639acd0a7f35667
陌生人 <1390903070@qq.com>
2023-09-01 09:32:15 +0800

    update

--- src/helpers.php
+++ src/helpers.php
@@ -1,33 +1,11 @@
 <?php
 
-use DreamCoders\DingBot;
+namespace DreamCoders\DingBot;
 
-if (!function_exists('dingNotice')) {
-    function dingNotice($robot = '')
+if (!function_exists('Ding')) {
+    function Ding($robot = 'default')
     {
-
-
-        dd(23456789345675566464564);
-        if ($robot) {
-            return (new DingBot(config('dingbot') ?? []))->useRobot();
-
-            return (new DingBot($app['config']['dingbot'] ?? []))->useRobot($robot)->getInstance();
-//            return app('dingtalk-notice')->useRobot($robot)->getInstance();
-        }
-        return app('dingtalk-notice')->useRobot()->getInstance();
+        return (new DingBot($robot));
     }
 }
 
-
-
-
-
-if (!function_exists('ding_notice')) {
-    function ding_notice($robot = '')
-    {
-        if ($robot) {
-            return app('dingtalk-notice')->useRobot($robot)->getInstance();
-        }
-        return app('dingtalk-notice')->useRobot()->getInstance();
-    }
-}
